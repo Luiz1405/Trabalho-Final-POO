1a359f8f38a187d614a5bc16af49cdf2628842bf
Luiz <kubaszewski001@hotmail.com>
2024-11-17 16:50:34 -0300

    Crindo os pacotes para seguir o modelo MVC e começando a criação da tela principal

---
+++ Trabalho Final POO/src/Model/GerenciadorDeAtletas.java
@@ -0,0 +1,17 @@
+package Model;
+import java.util.List;
+import java.util.ArrayList;;
+
+public class GerenciadorDeAtletas {
+    private List<Atleta> atletas;
+
+    public GerenciadorDeAtletas(){
+        this.atletas = new ArrayList<>();
+    }
+
+    //METODO PARA ADICIONAR ATLETA
+    //METODO PARA LISTAR OS ATLETAS(PODEMOS CRIAR UMA FILTRAGEM, POR EXEMPLO PUXAR APENAS POR JOGADORES DE BASQUETE)
+    //METODO PARA PESQUISAR ATLETA POR NOME
+    //METODO PARA AS ESTATISTICAS GERAIS, COMO POR EXEMPLO: Número total de atletas, Número de atletas por tipo, Somatório e medias dos salarios, somatorio de e media de algum atributo especifico.
+    //Podemos criar alguma metodo para listar os melhores jogadores de suas categorias.
+}
---
+++ Trabalho Final POO/src/Model/JogadorDeFutebol.java
@@ -0,0 +1,43 @@
+package Model;
+public class JogadorDeFutebol extends Atleta {
+
+    private String posicao;
+    private int golsmarcados;
+    private int assistencias;
+
+    public JogadorDeFutebol(String nome, int idade, String nacionalidade, double salario, String posicao, int golsmarcados, int assistencias){
+        super(nome, idade, nacionalidade, salario);
+
+        this.posicao = posicao;
+        this.golsmarcados = golsmarcados;
+        this.assistencias = assistencias;
+    }
+
+    public String getPosicao() {
+        return posicao;
+    }
+
+    public int getGolsmarcados() {
+        return golsmarcados;
+    }
+
+    public void setGolsMarcados(int GolsMarcados){
+        this.assistencias = golsmarcados;
+    }
+
+    public int getAssistencias() {
+        return assistencias;
+    }
+
+    public void setAssistencias(int assistencias) {
+        this.assistencias = assistencias;
+    }
+
+    //criar um metodo utilizando lambda para calcular o desempenho, com base em gols e assistencias.
+
+    @Override
+    public String toString(){
+        return String.format("[Posicao: %s, Gols marcados: %d, Assistencias: %d]", super.toString() , posicao, golsmarcados, assistencias);
+    }
+    
+}
