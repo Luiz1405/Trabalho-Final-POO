9417b7774e1f4d46c5deb0b551792dc417a50cec
Luiz Augusto de Souza Kubaszewski <116366008+Luiz1405@users.noreply.github.com>
2024-11-17 11:16:22 -0300

    Add files via upload
    Criando a estrutura básica do projeto e fazendo algumas anotações do que seria interessante fazer.

---
+++ Trabalho Final POO/src/Atleta.java
@@ -0,0 +1,47 @@
+public abstract class Atleta{
+    private String nome;
+    private int idade;
+    private String nacionalidade;
+    private double salario;
+    
+    public Atleta(String nome, int idade, String nacionalidade, double salario) {
+        this.nome = nome;
+        this.idade = idade;
+        this.nacionalidade = nacionalidade;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    //Fazer um método abstrato para criar estatisticas especificas de cada atleta
+    @Override
+    public String toString(){
+        return String.format("[Nome do atleta: %s, Idade do atleta: %d, Nacionalidade: %s, Salario: R$%.2f]", nome, idade, nacionalidade, salario);
+    }
+}
---
+++ Trabalho Final POO/src/JogadorDeEsportesEletronicos.java
@@ -0,0 +1,44 @@
+public class JogadorDeEsportesEletronicos extends Atleta {
+
+    private String time;
+
+    private int vitorias;
+    private int derrotas;
+
+    public JogadorDeEsportesEletronicos(String nome, int idade, String nacionalidade, double salario, String time, int vitorias, int derrotas){
+        super(nome, idade, nacionalidade, salario);
+        this.time = time;
+        this.vitorias = vitorias;
+        this.derrotas = derrotas;
+
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public int getVitorias() {
+        return vitorias;
+    }
+
+    public void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+
+
+    // metodo que calcula a taxa de vitoria
+
+    public String toString(){
+        return String.format( "[Time: %s, Vitorias: %d, Derrotas: %d]", super.toString(), time, vitorias, derrotas);
+    }
+
+    
+}
